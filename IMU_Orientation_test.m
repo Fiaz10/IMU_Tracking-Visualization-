db27138a6bf07f65b961b7db20d762fa84f31fd2
Md Fiaz Islam Bhuiyan <fiaz.islam@gmail.com>
2020-11-16 11:39:36 -0600

    Update IMU_Orientation_test.m

--- IMU_Orientation_test.m
+++ IMU_Orientation_test.m
@@ -1,6 +1,12 @@
+
+% This clears the port for reuse 
 delete(instrfind({'Port'}, {'COM6'}));
 
-
+% In this program we use the AHRS filter
+% An attitude and heading reference system (AHRS) consist of a 9-axis system that uses an accelerometer, gyroscope, and magnetometer
+% to compute orientation of the device. The ahrsfilter produces a smoothly changing estimate of orientation of the device,
+% while correctly estimating the north direction. The ahrsfilter has the ability to remove gyroscope bias and can also detect
+% and reject mild magnetic jamming.
 
 % GyroscopeNoise and AccelerometerNoise is determined from datasheet.
 GyroscopeNoiseMPU9250 = 3.0462e-06; % GyroscopeNoise (variance value) in units of rad/s
